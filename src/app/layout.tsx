478040bd2221be7af97047af792d30e8825525fd
Aidys Pushkunov <mywayinprogramming@gmail.com>
2023-10-28 20:11:07 +0700

    First initialization of the project...

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Telengit Shatra | Телеҥет Шатра",
+  description:
+    "Shatra is an intellectual board game of the Telengit people. The goal of this project is to create an Internet platform for the game “Shatra” in real time between two people or with artificial intelligence.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <>
+      <div className="text-red-700">Telengit Shatra</div>
+      <p className="text-gray-400">
+        Shatra is an intellectual board game of the Telengit people. The goal of
+        this project is to create an Internet platform for the game “Shatra” in
+        real time between two people or with artificial intelligence.
+      </p>
+    </>
+  );
+}
