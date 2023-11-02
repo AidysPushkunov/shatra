c6ac7168e3a78830891a6b8e1cb486345e9cb439
Aidys Pushkunov <mywayinprogramming@gmail.com>
2023-11-03 01:38:32 +0700

    Deleted text, added margin top and botom for board...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,15 +21,7 @@ export default function Home() {
 
   return (
     <>
-      <h1 className="flex text-[100px] text-red-700 justify-center">
-        Telengit Shatra
-      </h1>
-      <p className="text-gray-400 text-center max-w-[750px] m-[auto] my-1">
-        Shatra is an intellectual board game of the Telengit people. The goal of
-        this project is to create an Internet platform for the game “Shatra” in
-        real time between two people or with artificial intelligence.
-      </p>
-      <div className="flex items-center justify-center">
+      <div className="flex justify-center my-14">
         <BoardWidget board={board} setBoard={setBoard} />
       </div>
     </>
