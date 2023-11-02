5dcd121c4e42a864af68864c7d88c341fe16ebde
Aidys Pushkunov <mywayinprogramming@gmail.com>
2023-11-02 18:53:55 +0700

    Deleted svgr... We used variables for figures and render image with react-konva and use-image...

--- next.config.js
+++ next.config.js
@@ -2,27 +2,6 @@
 const nextConfig = {
   webpack: (config, options) => {
     config.externals = [...config.externals, { canvas: "canvas" }]; // required to make Konva & react-konva work
-
-    const fileLoaderRule = config.module.rules.find((rule) =>
-      rule.test?.test?.(".svg")
-    );
-
-    config.module.rules.push(
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/, // *.svg?url
-      },
-      {
-        test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
-        resourceQuery: { not: /url/ }, // exclude if *.svg?url
-        use: ["@svgr/webpack"],
-      }
-    );
-
-    fileLoaderRule.exclude = /\.svg$/i;
-
     return config;
   },
 };
