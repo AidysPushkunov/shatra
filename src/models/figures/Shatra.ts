6a0d7bbbbdd804883bda4c0efa071ae4a77b2607
Aidys Pushkunov <mywayinprogramming@gmail.com>
2023-11-05 04:41:36 +0700

    added rules move for figures...

--- src/models/figures/Shatra.ts
+++ src/models/figures/Shatra.ts
@@ -12,15 +12,36 @@ export class Shatra extends Figure {
   canMove(target: Cell): boolean {
     if (!super.canMove(target)) return false;
 
-    if (this.cell.isEmptyVertical(target)) {
+    const direction = this.cell.figure?.color === Colors.BLACK ? 1 : -1;
+
+    if (
+      target.y === this.cell.y + direction &&
+      target.x === this.cell.x &&
+      this.cell.board.getCell(target.x, target.y).isEmpty()
+    ) {
       return true;
     }
-    if (this.cell.isEmptyHorizontal(target)) {
+
+    if (
+      (target.y === this.cell.y + direction && target.x === this.cell.x + 1) ||
+      (target.y === this.cell.y + direction && target.x === this.cell.x - 1) ||
+      (target.y === this.cell.y && target.x === this.cell.x - 1) ||
+      (target.y === this.cell.y &&
+        target.x === this.cell.x + 1 &&
+        this.cell.isEnemy(target))
+    ) {
       return true;
     }
-    if (this.cell.isEmptyDiogonal(target)) {
-      return true;
-    }
+
     return false;
   }
+
+  /*
+    isFirstStep: boolean = true
+
+    moveFigure(target: Cell) {
+      super.moveFigure(target);
+      this.isFirstStep = false
+    }
+  */
 }
