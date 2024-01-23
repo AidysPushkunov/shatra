460ab36a2062bc345317ff5ce9af99877316e4b2
Aidys Pushkunov <mywayinprogramming@gmail.com>
2024-01-24 05:57:15 +0700

    changed config for deploy added ...

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
 
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(
+    `${process.env.PWD}/node_modules/canvas/build/Release:`
+  )
+) {
+  process.env.LD_LIBRARY_PATH = `${
+    process.env.PWD
+  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ""}`;
+}
+
 const nextConfig = {
   webpack: (config, options) => {
-    // For building on vercel: https://github.com/Automattic/node-canvas/issues/1779
-    if (
-      process.env.LD_LIBRARY_PATH == null ||
-      !process.env.LD_LIBRARY_PATH.includes(
-        `${process.env.PWD}/node_modules/canvas/build/Release:`
-      )
-    ) {
-      process.env.LD_LIBRARY_PATH = `${
-        process.env.PWD
-      }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ""}`;
-    }
     /**
      * Critical: prevents " ⨯ ./node_modules/canvas/build/Release/canvas.node
      * Module parse failed: Unexpected character '�' (1:0)" error
