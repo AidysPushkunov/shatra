28d696d7816f1888d58105ec38a5fd983a5c8567
Aidys Pushkunov <mywayinprogramming@gmail.com>
2024-04-27 01:44:31 +0700

    changed cors href for connected server

--- src/contexts/socketContext.tsx
+++ src/contexts/socketContext.tsx
@@ -13,11 +13,11 @@ export const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
     const [socket, setSocket] = useState<Socket | null>(null);
 
     useEffect(() => {
-        const newSocket = io('https://45.147.179.12:5000', {
+        const newSocket = io('http://45.147.179.12:80', {
             withCredentials: true,
             transports: ['websocket'],
             extraHeaders: {
-                'Access-Control-Allow-Origin': 'https://shatra.vercel.app/',
+                'Access-Control-Allow-Origin': 'https://shatra.vercel.app',
             },
         });
 
